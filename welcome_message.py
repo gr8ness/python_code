95b8abce345711904a3b9bc550cc8f56ad45561b
Deshone H <deshonehenry@gmail.com>
2022-04-12 21:38:52 -0400

    Create welcome_message.py

---
+++ welcome_message.py
@@ -0,0 +1,8 @@
+# Allow user to input their First Name and assign that input to a variable
+firstname = input("Please enter your last name. ")
+ 
+# Allow user to input their Last Name and assign that input to a variable
+lastname = input("Please enter your last name. ")
+
+# Print a welcome message that includes the user's First Name and Last Name using the variables.
+print("Hello", firstname, lastname, "My name is Py, and I will be your instructor for python. Now, let me show you.")
