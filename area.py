99ef8d12177693ee8820be34db7254f010d58cae
Deshone Henry <deshonehenry@gmail.com>
2022-04-14 17:13:46 -0400

    new file

---
+++ area.py
@@ -0,0 +1,11 @@
+# Allow user input for Length and set to a variable
+l = input("Enter a number for length: ")
+
+# Allow user input for Width and set to a variable
+w = input("Enter a number for width: ")
+
+# Set Length X Width to a new variable
+l2 = l
+w2 = w
+# Print a message that the Square Footage of the room is: Square Foot Variable
+print( "Total area of length and width is: " , l2*w2)
