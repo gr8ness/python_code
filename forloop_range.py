806bf9cf3acbf969975979ed1600a94fbc827ef7
Deshone H <deshonehenry@gmail.com>
2022-04-14 17:08:28 -0400

    Create forloop_range.py

---
+++ forloop_range.py
@@ -0,0 +1,12 @@
+'''Use the range function to generate a list of 20 or more elements. 
+Shuffle and iterate through the list printing each element and its square to the screen'''
+
+#num = range(20)
+
+for e in range(1,20):
+  print(e, e** 2)
+
+"""squares = []
+for i in range(5):
+    squares.append(i**2)
+    print(i)"""
