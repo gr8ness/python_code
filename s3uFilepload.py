83d148e53e82a7ad0bed1a6c9a7cdfabf03a7c8a
Deshone H <deshonehenry@gmail.com>
2022-04-19 19:31:22 -0400

    added a way to upload multiple files

--- s3uFilepload.py
+++ s3uFilepload.py
@@ -1,8 +1,16 @@
 import boto3
 import os
+import glob
 
-s3 = boto3.resource('s3')
-s3.Bucket('mydemobucket4767822315489').upload_file('/home/ec2-user/environment/s3Upload.py', 's3Upload.py') #Req: bucket name, file path and file name as the key
+#s3 = boto3.resource('s3')
+#s3.Bucket('mydemobucket4767822315489').upload_file('/home/ec2-user/environment/s3Upload.py', 's3Upload.py') #Req: bucket name, file path and file name as the key
+
+
+files = glob.glob("*.py") #uploads all files ending in .py with glob extentsion
+
+for file in files: #for loop that uploads the files to the bucket
+    s3 = boto3.resource('s3')
+    s3.Bucket('mydemobucket4767822315489').upload_file(file, file) #bucket name (file,file) 
 
 response = "file uploaded"
 
